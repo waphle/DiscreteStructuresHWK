8c839710ba5e013530bdfb463daf558e737cd555
Jeff Zhang <jz1766american.edu>
2020-10-09 11:32:46 -0400

    Update permutationsOfNumbers.java

--- permutationsOfNumbers.java
+++ permutationsOfNumbers.java
@@ -1,4 +1,11 @@
 // Initial Commit
 public class permutationsOfNumbers { 
-
+   public static void main(String[] args) {
+      int list [] = {1, 2, 3, 4, 5}; // set of numbers the program can choose from
+      int SetOfDigits = list.length;
+      int n = 3; // print out three of the numbers from the list
+      int perm = fact(SetOfDigits) / fact(SetOfDigits - n); // Permutation calculation, fact = factorial
+      
+      System.out.println("Possibilities for numbers with" +n+ "digits: " +perm);
+   }
 }
