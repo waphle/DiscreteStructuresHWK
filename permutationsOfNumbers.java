b3efa62f49d7a3f4bb64234aaa9548bcb237689b
Jeff Zhang <jz1766american.edu>
2020-10-09 14:24:30 -0400

    Created VirginaPlates.java, move on from permutations for now

--- permutationsOfNumbers.java
+++ permutationsOfNumbers.java
@@ -2,24 +2,24 @@ public class permutationsOfNumbers {
    public static void main(String[] args) {
       int list[] = {2, 3, 4, 5, 6}; // set of numbers the program can choose from
       int SetOfDigits = list.length;
-      int t = 3; // print out three of the numbers from the list
-      int data[] = new int[t];
+      int SomeNum = 3; // print out three of the numbers from the list
+      int data[] = new int[SomeNum];
       
-      System.out.println("Some possibilities with " +t+ " digits: ");
-      printPermutations(list, data, SetOfDigits - 1, 0, t);
+      System.out.println("Some possibilities with " +SomeNum+ " digits: ");
+      printPermutations(list, data, SetOfDigits - 1, 0, SomeNum);
    }
    
-   public static void printPermutations(int arr[], int data[], int end, int index, int t) {
-      if (index == t) {
-         for (int i = 0; i < t; i++)
+   public static void printPermutations(int arr[], int data[], int end, int index, int SomeNum) {
+      if (index == SomeNum) {
+         for (int i = 0; i < SomeNum; i++)
          System.out.print(data[i]);
          System.out.println(""); // Line break with each permutation output
          return;
       }   
         
-      for (int k = 0; k <= end && end-k >= t - index; k++) {
+      for (int k = 0; k <= end && end-k >= SomeNum - index; k++) {
          data[index] = arr[k];
-         printPermutations(arr, data, end, index + 1, t);
+         printPermutations(arr, data, end, index + 1, SomeNum);
       }
       
       
