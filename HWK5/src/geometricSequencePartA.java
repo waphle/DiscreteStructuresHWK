a4e0f84205588e87887a7958345e3fa090284a5e
Jeff Zhang <jz1766american.edu>
2020-11-04 16:16:08 -0500

    Updated partA_Geo

--- HWK5/src/geometricSequencePartA.java
+++ HWK5/src/geometricSequencePartA.java
@@ -1,28 +1,19 @@
-import java.io.*; 
-  
 class partA_Geo { 
-      
-    // function to calculate sum of  
-    // geometric series 
-    static float sumOfGP(float a, float r, int n) 
+    static void printGP(int a, int r, int n) 
     { 
-        float sum = 0;  
-        for (int i = 0; i < n; i++) 
-        { 
-            sum = sum + a; 
-            a = a * r; 
+        int curr_term; 
+        for (int i = 0; i < n; i++) { 
+            curr_term = a * (int)Math.pow(r, i); 
+            System.out.print(curr_term + " "); 
         } 
-        return sum; 
     } 
   
-    // driver function 
-    public static void main(String args[]) 
+    // Driver code 
+    public static void main(String[] args) 
     { 
-        int a = 1; // first term 
-        float r = (float)(1/2.0) ;// common ratio 
-        int n = 10 ; // number of terms 
-          
-        System.out.printf("%.5f",(sumOfGP(a, r, n))); 
+        int a = 2; // starting number 
+        int r = 3; // Common ratio 
+        int n = 5; // N th term to be find 
+        printGP(a, r, n); 
     } 
-      
-}
+} 
