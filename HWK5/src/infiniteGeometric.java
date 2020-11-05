0460667660dc02db0d4f664d56fa790a9dd9d8af
Jeff Zhang <jz1766american.edu>
2020-11-04 20:02:43 -0500

    Update infiniteGeometric.java

--- HWK5/src/infiniteGeometric.java
+++ HWK5/src/infiniteGeometric.java
@@ -9,18 +9,18 @@ public class infiniteGeometric {
 		System.out.println("Enter the first number: ");
 		double first = scan.nextDouble();
 		
-		System.out.println("Enter the ratio: ");
+		System.out.println("Enter the common ratio: ");
 		double ratio = scan.nextDouble();
 		
 		System.out.println("Enter the number of terms: ");
 		int term = scan.nextInt();
   
-		// If the ratio is greater than 1, there is no sum for the geometric sequence
+		// If the common ratio is greater than 1, there is no sum for the geometric sequence
 		if (ratio >= 1) {
 			System.out.println("The infinite sequence does not have a sum.");
 		}
 			
-		// If the ratio is less than 1, there is a sum. Calculate the sum and print. 
+		// If the common ratio is less than 1, there is a sum. Calculate the sum and print. 
 		else {
 			sum=first/(1-ratio);
 			
